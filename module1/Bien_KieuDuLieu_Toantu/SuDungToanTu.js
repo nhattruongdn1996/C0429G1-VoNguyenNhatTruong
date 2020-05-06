6747341874db7da1adc262d4d339a4a07566f2f5
nhattruongdn1996 <nhattruongdn1996@gmail.com>
2020-05-06 15:28:59 +0700

    Sử dụng toán tử

---
+++ module1/Bien_KieuDuLieu_Toantu/SuDungToanTu.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt("Nhập chiều rộng bạn muốn");
+let inputHeight = prompt("Nhập chiều cao bạn muốn");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write('Diện tích của hình chữ nhận là: ' + area);
