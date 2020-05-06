994ebffe41bb38b084af04f2a81117b13762e9f0
nhattruongdn1996 <nhattruongdn1996@gmail.com>
2020-05-06 16:00:43 +0700

    Biến, kiểu dữ liệu và toán tử

---
+++ module1/Bien_KieuDuLieu_Toantu/Bien_KieuDuLieu_ToanTu.js
@@ -0,0 +1,19 @@
+let a = prompt("Nhập điểm Vật lý");
+let b = prompt("Nhập điểm Hóa học");
+let c = prompt("Nhập điểm Sinh học");
+let a1 = parseInt(a);
+let b1 = parseInt(b);
+let c1 = parseInt(c);
+let d = (a1+b1+c1)/3;
+let e = a1 + b1 + c1;
+
+let a2 = prompt("Nhập độ C cần đổi:");
+let C = parseInt(a2);
+let F = (9*C +160)/5;
+
+let a3 = prompt("Nhập bán kính hình tròn:");
+let r = parseInt(a3);
+let S = r*r*3.14;
+let Cv = 2*r*3.14;
+
+
